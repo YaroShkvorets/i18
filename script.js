0ea6dd9bf05eb28c6fe16778e5346cdf26ca44f2
YaroShkvorets <shkvorets@gmail.com>
2021-11-02 01:32:42 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+
+function getDiff(before, after)
+{
+    console.log(before, typeof before, after, typeof after)
+    if(before == after) return null;
+    if(!before) return after;
+    if(typeof after == "string") return after;
+    var res = {};
+    for(var key of Object.keys(after)){
+        var diff = getDiff(before[key], after[key]);
+        if(diff) res[key] = diff;
+    }
+    return Object.keys(res).length ? res : null;
+}
+
+function diff()
+{
+    var beforeJson = document.getElementById("before").value
+    var afterJson = document.getElementById("after").value
+
+    eval(`var before = ${beforeJson}`);
+    eval(`var after = ${afterJson}`);
+
+    console.log('parsing ', before , ' to ', after)
+
+    const diff = getDiff(before, after);
+
+    document.getElementById("result").value = diff ? JSON.stringify(diff, null, "  ") : "NO CHANGES"
+}
+
+
+
+
+/*
+{
+  "about": {
+    "hero": {
+      "title": "Support the EOS projects you love.",
+      "description": "Pomelo is an open-source crowdfunding platform that multiplies your contributions."
+    },
+    "info": {
+      "explore_grants": "EXPLORE GRANTS",
+      "item_1": "Discover exciting projects",
+      "item_2": "Support the ones you love",
+      "item_3": "Watch contributions multiply",
+      "description": "Inspired by Gitcoin, contributions are matched from a pool of funds provided by the EOS Network Foundation."
+    },
+  }
+}
+
+
+{
+  "about": {
+    "hero": {
+      "title": "EDITED",
+      "description": "Pomelo is an open-source crowdfunding platform that multiplies your contributions."
+    },
+    "info": {
+      "explore_grants": "EXPLORE GRANTS",
+      "item_2": "Support the ones you love",
+      "item_3": "Watch contributions multiply",
+      "description": "Inspired by Gitcoin, contributions are matched from a pool of funds provided by the EOS Network Foundation."
+    },
+  }
+}
+*/
